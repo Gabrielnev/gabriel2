5b13ecdf280a596f724eb650ab17aaa2b0025432
vtr31-13hg <144157603+vtr31-13hg@users.noreply.github.com>
2023-10-02 10:07:46 -0300

    update.index
    update index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href=" estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="corpo">
+    <h1>escolha um jogo</h1>
+    <table class = "listagem">
+        <tr><td>nome<td>nome<td>adm
+        <tr><td>nome<td>nome<td>adm
+        <tr><td>nome<td>nome<td>adm
+        <tr><td>nome<td>nome<td>adm
+         
+    </table>
+    </div>
+</body>
+</html>
